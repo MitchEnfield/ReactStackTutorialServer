fdd807417f764132663f466c74420f3afcb55350
Mitchell Enfield <Menfield@driversalert.com>
2017-09-06 17:39:56 -0400

    User signs up (Need to be fixed)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 import Promise from 'bluebird'
+
 import auth from "./routes/auth";
+import users from "./routes/users";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-mongoose.promise = Promise;
+mongoose.mpromise = Promise;
 mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
 app.use("/api/auth", auth);
+app.use("/api/users", users)
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
